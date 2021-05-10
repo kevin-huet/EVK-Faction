9e2b07c216da0df6d24080ec3ea5aab7fecffb5d
kevin <kevin.huet@epitech.eu>
2021-05-10 14:09:25 +0200

    add role check for commands

--- src/main/java/com/github/kevinhuet/evkfaction/Command/Faction/RenameFactionCommand.java
+++ src/main/java/com/github/kevinhuet/evkfaction/Command/Faction/RenameFactionCommand.java
@@ -1,13 +1,19 @@
 package com.github.kevinhuet.evkfaction.Command.Faction;
 
 import com.github.kevinhuet.evkfaction.Command.SubCommand;
+import com.github.kevinhuet.evkfaction.Entity.FactionPlayer;
+import com.github.kevinhuet.evkfaction.Entity.Role;
+import com.github.kevinhuet.evkfaction.Service.FactionPlayerManager;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
 public class RenameFactionCommand implements SubCommand {
     @Override
     public void onCommand(Player player, Command command, String[] args) {
+        FactionPlayer factionPlayer = FactionPlayerManager.getInstance().getPlayerFaction(player);
 
+        if (factionPlayer == null || !factionPlayer.getRole().isAtMost(Role.OFFICER))
+            return;
     }
 
     @Override
--- src/main/java/com/github/kevinhuet/evkfaction/Command/Faction/ShowFactionCommand.java
+++ src/main/java/com/github/kevinhuet/evkfaction/Command/Faction/ShowFactionCommand.java
@@ -1,12 +1,22 @@
 package com.github.kevinhuet.evkfaction.Command.Faction;
 
 import com.github.kevinhuet.evkfaction.Command.SubCommand;
+import com.github.kevinhuet.evkfaction.Entity.Faction;
+import com.github.kevinhuet.evkfaction.Entity.FactionPlayer;
+import com.github.kevinhuet.evkfaction.Entity.Role;
+import com.github.kevinhuet.evkfaction.Service.FactionManager;
+import com.github.kevinhuet.evkfaction.Service.FactionPlayerManager;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
 public class ShowFactionCommand implements SubCommand {
     @Override
     public void onCommand(Player player, Command command, String[] args) {
+        Faction faction = null;
+
+        if (args.length < 2)
+            return;
+        faction = FactionManager.getInstance().getFactionByName(args[1]);
 
     }
 
